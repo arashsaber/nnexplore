e816be3681191cde186f084db28f85546d921d5e
arash <arash.saber.tehrani@gmail.com>
2018-02-23 12:35:35 -0800

    vae

---
+++ VAE/model-v2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+The file contains a Variational Autoencoder in tflearn.
+
+Copyright (c) 2017
+Licensed under the MIT License (see LICENSE for details)
+Written by Arash Tehrani
+"""
+#   ---------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
